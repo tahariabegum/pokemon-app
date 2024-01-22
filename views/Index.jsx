03171b53ab5f3e4bc8d1935dbf4e78a16aab37df
tahariabegum <taharia97@gmail.com>
2024-01-22 00:12:42 -0500

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Index( {pokemon} ) {
     return (
-        <h1> See All the Pokemon! </h1>
+        <div>
+            <h1> See All the Pokemon! </h1>
+            <ul>
+                {pokemon.map((pokemon,i) => 
+                <li key = {i}>
+                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1).toLowerCase()}
+                </li>
+                )}
+            </ul>
+        </div>
     )
 }
